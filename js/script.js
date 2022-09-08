d5438b1281c7692a9888a52af0e785c99cc06157
Miriam Torrens <m.torrens14@gmail.com>
2022-09-08 12:48:19 +0200

    windows.location

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@ const textMenu = document.getElementById("menu").innerHTML;
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("menu-div-active");
   if (menu.classList.contains("menu-div-active")) {
-    menuIcon.setAttribute("src", "../img/iconos_svg/x.svg");
+    if (
+      window.location ===
+      "https://miriamtorrens.github.io/web-hotel-miranda/index.html"
+    ) {
+      menuIcon.setAttribute("src", "./img/iconos_svg/x.svg");
+    } else {
+      menuIcon.setAttribute("src", "../img/iconos_svg/x.svg");
+    }
     menu.innerHTML = textMenu;
   } else {
     menuIcon.setAttribute("src", "../img/iconos_svg/menu.svg");
