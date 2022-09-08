7e1e63957921fd95aa5479e2bc104e3f4a91071d
Miriam Torrens <m.torrens14@gmail.com>
2022-09-08 13:14:15 +0200

    delete html folder

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const textMenu = document.getElementById("menu").innerHTML;
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("menu-div-active");
   if (menu.classList.contains("menu-div-active")) {
-    menuIcon.setAttribute("src", "../img/iconos_svg/x.svg");
+    menuIcon.setAttribute("src", "./img/iconos_svg/x.svg");
     menu.innerHTML = textMenu;
   } else {
-    menuIcon.setAttribute("src", "../img/iconos_svg/menu.svg");
+    menuIcon.setAttribute("src", "./img/iconos_svg/menu.svg");
     menu.innerHTML = "";
   }
 });
